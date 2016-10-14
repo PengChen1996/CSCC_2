a8145fc9c8d14daa26c7648a4ca30bc2fa638fa8
FlyingWWS <2601349541@qq.com>
2016-10-14 23:22:52 +0800

    login

--- src/main/java/com/zlzkj/app/controller/UserController.java
+++ src/main/java/com/zlzkj/app/controller/UserController.java
@@ -17,12 +17,12 @@ public class UserController extends BaseController{
 	@Autowired
 	private UserService userService;
 	
+	//注册
 	@RequestMapping(value={"user/userdata_entry_page"})
 	public String data_entry_page() {
 		System.out.println("user/userdata_entry_page");
 		return "user/userdata_entry";
 	}
-	
 	@RequestMapping(value={"user/userdata_entry"})
 	public String data_entry(Model model,HttpServletRequest request,HttpServletResponse response,
 			User user) {
@@ -32,5 +32,20 @@ public class UserController extends BaseController{
 		user.setPassword("111111");
 		userService.userdata_entry(user);
 		return "user/userdata_entry";
+	}
+	//登录
+	@RequestMapping(value={"user/login_page"})
+	public String login_page() {
+		System.out.println("user/login_page");
+		return "user/login";
+	}
+	@RequestMapping(value={"user/login"})
+	public String login(Model model,HttpServletRequest request,HttpServletResponse response,
+			User user) {
+		System.out.println("user/login");
+		user.getAccount();
+		user.getPassword();
+		userService.login(user.getAccount(),user.getPassword(),0);
+		return "user/login";
 	}
 }
--- src/main/java/com/zlzkj/app/service/UserService.java
+++ src/main/java/com/zlzkj/app/service/UserService.java
@@ -7,13 +7,24 @@ import org.springframework.transaction.annotation.Transactional;
 import com.zlzkj.app.mapper.UserMapper;
 import com.zlzkj.app.model.User;
 
+import com.zlzkj.core.mybatis.SqlRunner;
+
+
 @Service
 @Transactional
 public class UserService {
 	@Autowired
 	private UserMapper mapper;
+	@Autowired
+	private SqlRunner sqlRunner;
 
 	public int userdata_entry(User user){
 		return mapper.insertSelective(user);
+	}
+	
+	public boolean login(String account,String password,int status){
+		String sql = "select * from x_user where account="+account+",password="+password+"and status="+status+"";
+		boolean islogin = sqlRunner.select(sql) != null;
+		return islogin;
 	}
 }
